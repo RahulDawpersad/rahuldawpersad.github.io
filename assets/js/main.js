a99bdaf76ada73da657d9a7a10928ae9b96e237a
RahulDawpersad <135977072+RahulDawpersad@users.noreply.github.com>
2023-11-08 17:01:50 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,3 +34,65 @@ setTimeout(function () {
 		$('.scrolly').scrolly();
 
 })(jQuery);
+
+// Cookie Functionality
+document.addEventListener("DOMContentLoaded", function() {
+    var cookiePopup = document.getElementById("cookie-popup");
+    var acceptCookiesButton = document.getElementById("accept-cookies");
+    var declineCookiesButton = document.getElementById("decline-cookies");
+  
+    acceptCookiesButton.addEventListener("click", function() {
+      cookiePopup.style.display = "none";
+      setCookie("cookiesAccepted", true, 365);
+      // Store the user's data here
+      var userData = {
+        name: "John Doe",
+        email: "johndoe@example.com",
+        // Add more data fields as needed
+      };
+      localStorage.setItem("userData", JSON.stringify(userData));
+    });
+  
+    declineCookiesButton.addEventListener("click", function() {
+      cookiePopup.style.display = "none";
+      setCookie("cookiesAccepted", false, 365);
+      // Clear the user's data if they decline cookies
+      localStorage.removeItem("userData");
+    });
+  
+    if (!getCookie("cookiesAccepted")) {
+      cookiePopup.style.display = "block";
+    }
+  
+    function setCookie(name, value, days) {
+      var expires = "";
+      if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+      }
+      document.cookie = name + "=" + (value || "") + expires + "; path=/";
+    }
+  
+    function getCookie(name) {
+      var nameEQ = name + "=";
+      var ca = document.cookie.split(';');
+      for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+      }
+      return null;
+    }
+  });
+  
+  // Icon Functionality
+  function iconBtn(){
+    window.location.href = './assets/html/cookie.html';
+  }
+
+
+
+
+
+
