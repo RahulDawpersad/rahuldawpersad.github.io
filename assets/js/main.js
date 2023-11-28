2e21c62bb639f1c84b84e7e19bd69557bd45149c
RahulDawpersad <135977072+RahulDawpersad@users.noreply.github.com>
2023-11-28 16:49:02 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -91,7 +91,23 @@ document.addEventListener("DOMContentLoaded", function() {
     window.location.href = 'cookie.html';
   }
 
+ // On SCROLL Functionality
+  //create the intersection observer
+const observer = new IntersectionObserver((entries)=>{
+  entries.forEach((entry) => {   //it can observe multiple entries at the same time
+      console.log(entry)
+      if(entry.isIntersecting){
+          entry.target.classList.add('show');
+      }else{
+          entry.target.classList.remove('show');
+      }
+  });
+});
+//grab all the elements that have that specific class.
+const hiddenElements = document.querySelectorAll('.hidden');
 
+//tell it what to observe
+hiddenElements.forEach((el)=> observer.observe(el)); //Tell the observer to observe
 
 
 
